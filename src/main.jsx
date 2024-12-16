a65551ae469c4c837fc4adbcc3272c8a603ad8c8
felipe giusti <felipeegiusti@gmail.com>
2024-12-16 13:41:49 -0300

    Componentes e paginas

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// import { Toaster } from 'react-hot-toast';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+import { Routes } from './routes';
+
+import { GlobalStyle } from './styles/global.js'
+import { ThemeProvider } from 'styled-components'
+import theme from './styles/theme';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Routes />
+    </ThemeProvider>
+  </React.StrictMode>,
 )
